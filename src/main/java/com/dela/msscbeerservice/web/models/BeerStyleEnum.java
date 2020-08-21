eb7d29bc820fc422b29ad4556582d2d694e87be7
delalic <delalicharis@gmail.com>
2020-08-21 21:27:46 +0200

    Add BeerDto and BeerPageList models

---
+++ src/main/java/com/dela/msscbeerservice/web/models/BeerStyleEnum.java
@@ -0,0 +1,13 @@
+package com.dela.msscbeerservice.web.models;
+
+public enum BeerStyleEnum {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE
+}
